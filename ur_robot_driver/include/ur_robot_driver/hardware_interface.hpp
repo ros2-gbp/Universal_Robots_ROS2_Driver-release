c3dccaa4fff58fb07bd83c9375b064b91c3725c4
Felix Exner <feex@universal-robots.com>
2026-04-09 14:05:33 +0200

    Rebase from 'upstream'

--- ur_robot_driver/include/ur_robot_driver/hardware_interface.hpp
+++ ur_robot_driver/include/ur_robot_driver/hardware_interface.hpp
@@ -115,11 +115,11 @@ struct Quaternion
 class URPositionHardwareInterface : public hardware_interface::SystemInterface
 {
 public:
-  RCLCPP_SHARED_PTR_DEFINITIONS(URPositionHardwareInterface);
+  RCLCPP_SHARED_PTR_DEFINITIONS(URPositionHardwareInterface)
   URPositionHardwareInterface();
   virtual ~URPositionHardwareInterface();
 
-  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& system_info) final;
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareComponentInterfaceParams& params) final;
 
   std::vector<hardware_interface::StateInterface> export_state_interfaces() final;
 
@@ -260,10 +260,15 @@ protected:
   urcl::vector6d_t passthrough_trajectory_accelerations_;
   double passthrough_trajectory_time_from_start_;
 
-  // payload stuff
+  // Payload stuff
   urcl::vector3d_t payload_center_of_gravity_;
   double payload_mass_;
   double payload_async_success_;
+
+  // Friction model parameters
+  urcl::vector6d_t friction_model_viscous_;
+  urcl::vector6d_t friction_model_coulomb_;
+  double friction_model_async_success_;
 
   // force mode parameters
   urcl::vector6d_t force_mode_task_frame_;
@@ -324,6 +329,9 @@ protected:
   const std::string TOOL_CONTACT_GPIO = "tool_contact";
 
   std::unordered_map<std::string, std::unordered_map<std::string, bool>> mode_compatibility_;
+
+  std::unique_ptr<urcl::rtde_interface::DataPackage> data_package_buffer_;
+  std::function<bool()> get_data_package;
 };
 }  // namespace ur_robot_driver
 
